66af5fcb473d8f7ddf72e42ada0bb2d18350071c
RoyXun <963877037@qq.com>
2017-03-13 12:01:13 +0800

    修改实现

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ## 简介
 
 项目中经常遇到slider插件需要给用户提供一个可输入数值的输入框，`angularjs-slider`通过输入框输入数值没有限定最大最小值，会导致各种异常场景出现。
-`angular-slider-input`就是为这样的使用场景写的简单指令，可以通过滑动条滑动，点击修改绑定数值，也可以在输入框内输入具体数值，当输入数值合法时会通知插件渲染滑块位置。
+`angular-slider-input`就是为这样的使用场景写的简单指令，当操作滑块时，输入框的值会和滑块值保持一致；而当在输入框内输入数值时，失去焦点时会校验数值合法性。
 
 
 ![screenshot](./screenshot.jpg);
@@ -14,21 +14,22 @@
 
 ```javascript
     angular.module('yourApp', ['rzModule', 'rx.sliderInput'])
-        .controller['$scope', 'EventBus', function($scope, EventBus) {
+        .controller['$scope', function($scope) {
             $scope.slider = {
                 value: 10,
                 options: {
                     floor: 0,
                     ceil: 100,
+                    minLimit: 0,
+                    maxLimit: 100,
                     step: 1,
                     showSelectionBar: true
                 }
             };
 
-            // inform slider plugin to render
-            EventBus.on('rx.slider.input.valid', function(e) {
-                $scope.slider.value = e.data;
-            });
+            $scope.resetSlider = function(value) {
+                $scope.slider.value = value;
+            };
         }];
 ```
 
@@ -38,12 +39,12 @@
 <rzslider class="custom-slider" rz-slider-model="slider.value" rz-slider-options="slider.options"></rzslider>
 
 <!-- angular-slider-input -->
-<input type="text" rx-slider-input="slider.value" ng-model="foo" slider-id="custom_id" min="slider.floor" max="slider.ceil">
-
+<input type="text" rx-slider-input="slider.value" ng-model="foo" 
+     min="slider.minLimit" 
+     max="slider.maxLimit"
+     step="slider.step"
+     on-blur="resetSlider(value)">
 ```
 
-## 注意点
 
-- 当输入值合法时会通过EventBus通知slider插件渲染，在失去焦点时会将输入值改为合法值
-- 输入框的model不要和slider绑定同一个model，不要依赖输入框的model做其他任何事情
-- 暂时只支持非负整数段输入
+
--- index.js
+++ index.js
@@ -1,83 +1,69 @@
-angular.module('rx.sliderInput', []).service('EventBus', function() {
-    var eventMap = {};
-
-    return {
-        on: function (eventType, handler) {
-            if (!eventMap[eventType]) {
-                eventMap[eventType] = [];
-            }
-            eventMap[eventType].push(handler);
-        },
-
-        off: function (eventType, handler) {
-            for (var i = 0; i < eventMap[eventType].length; i++) {
-                if (eventMap[eventType][i] === handler) {
-                    eventMap[eventType].splice(i, 1);
-                    break;
-                }
-            }
-        },
-
-        fire: function (event) {
-            var eventType = event.type;
-            if (eventMap[eventType]) {
-                for (var i = 0; i < eventMap[eventType].length; i++) {
-                    eventMap[eventType][i](event);
-                }
-            }
-        }
-    };
-})
+angular.module('rx.sliderInput', [])
 /**
  *  @example:
- *  <input type="text" rx-slider-input="slider.value" ng-model="foo" min="1" max="10" slider-id="uniqueId">
+ *  <input type="text" class="form-control" 
+ *      ng-model="fooInput" 
+ *      rx-slider-input="sliderValue" 
+ *      min="0" 
+ *      max="100" 
+ *      step="2" 
+ *      on-blur="resetSlider(value)">
  */
 
-.directive('rxSliderInput', ['EventBus', function(EventBus) {
+.directive('rxSliderInput', function() {
     return {
         restrict: 'A',
         require: 'ngModel',
+        scope: {
+            min: '=', 
+            max: '=',
+            step: '=',
+            slider: '=rxSliderInput',
+            onBlur: '&'
+        },
         link: function(scope, element, attrs, ngModelCtrl) {
-            var min, max, lastValidValue;
+            var min = scope.min;
+            var max = scope.max;
+            var step = scope.step || 1;
 
-            min = parseFloat(scope.$eval(attrs.min));
-            max = parseFloat(scope.$eval(attrs.max));
-
-            //Do not use $setViewValue to set viewValue here, because it will trigger $parse pipeline.
-            ngModelCtrl.$parsers.push(function(input) {
-                input = parseFloat(input);
-                if (isNaN(input)) {
-                    ngModelCtrl.$viewValue = '';
-                    ngModelCtrl.$render();
-                } else if (input >= min && input <= max) {
-                    if (lastValidValue !== input) {
-                        //Use EventBus to inform slider to change model 
-                        EventBus.fire({
-                            type: 'rx.slider.input.valid',
-                            data: input,
-                            sliderId: attrs.sliderId
-                        });
-                    }
-                    lastValidValue = input;
-                    ngModelCtrl.$viewValue = input;
-                    ngModelCtrl.$render();
-                } 
-                return lastValidValue;
-            });
-
-            element.on('blur', function() {
-                if (ngModelCtrl.$viewValue !== lastValidValue) {
-                    ngModelCtrl.$setViewValue(lastValidValue);
-                    ngModelCtrl.$render();
-                    scope.$apply();
+            var getValidValue = function(input, min, max, step) {
+                var value = parseFloat(input) || 0;
+                //determine if input is in the valid range
+                if (value < min) {
+                    value = min;
+                } else if (value > max) {
+                    value = max;
                 }
-            });
 
-            scope.$watch(attrs.rxSliderInput, function(value) {
-                lastValidValue = value;
-                ngModelCtrl.$viewValue = value;
+                //determin if input is valid 
+                value = min + Math.round((value - min) / step) * step;
+                if (value < min) {
+                    value += step;
+                } else if (value > max) {
+                    value -= step;
+                }
+
+                return value;
+            };
+
+            // the input value should be consistent with the slider model.
+            scope.$watch('slider', function(value) {
+                ngModelCtrl.$setViewValue(value);
                 ngModelCtrl.$render();
             });
+
+            // validate value and reset slider when blur
+            element.on('blur', function() {
+                var value = getValidValue(ngModelCtrl.$viewValue, min, max, step);
+
+                if (value != scope.slider) {
+                    scope.onBlur({value: value});
+                }
+
+                ngModelCtrl.$setViewValue(value);
+                ngModelCtrl.$render();
+                scope.$apply();
+            });
         }
-    }
-}]);
+    };
+});
